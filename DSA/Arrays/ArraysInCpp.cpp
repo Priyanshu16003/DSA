a376c9db4b852f2a4393dbbc57626d1db8dceb50
Priyanshu <pteof19@gmail.com>
2024-06-04 22:50:16 +0530

    2d Arrays in C++

--- DSA/Arrays/ArraysInCpp.cpp
+++ DSA/Arrays/ArraysInCpp.cpp
@@ -10,5 +10,15 @@ int main() {
         cout<<arr[i]<<"->";
     }
     
+    // 2 Dimentional Array
+    int array[3][3] = { {1,2,3}, {4,5,6}, {7,8,9}};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout<<array[i][j]<<", ";
+        }
+        cout<<endl;
+    }
+    
+
     return 0;
 }
